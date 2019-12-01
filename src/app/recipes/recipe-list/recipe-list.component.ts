69bbc4cf8f76ff36aa779b60ce5886b31aeafd49
Hitasvi <hitasvi.sutar@gmail.com>
2019-12-01 17:53:32 +0530

    added code for recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -12,11 +12,14 @@ export class RecipeListComponent implements OnInit {
   new Recipe('Kurkuri Bhindi','Kurkuri Bhindi is a spicy fried snack or main course dish made using Okra, gram flour and spices.',
   'https://www.plattershare.com/common/uploaded_files/recipe/5b2224e6108f5_pb.jpg')
 ];
-
+@Output() recipeWasSelected=new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
 
+  }
+  onRecipeSelected(recipe:Recipe){
+this.recipeWasSelected.emit(recipe);
   }
 
 }
